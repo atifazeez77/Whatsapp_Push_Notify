6e2e98658816c1dd2937229862ce9114e2144ae5
ATIF AZEEZ <atifazeez77@gmail.com>
2020-07-11 18:51:10 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+from twilio.rest import Client
+
+account_sid = os.environ['account_sid']
+auth_token = os.environ['auth_token']
+to_whatsapp_no = os.environ['to_whatsapp_no']
+
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+                              body='Yay! Push event triggered in master branch',
+                              from_='whatsapp:+14155238886',
+                              to='whatsapp:'+to_whatsapp_no
+                          )
+
+print("Message ID:",message.sid)
